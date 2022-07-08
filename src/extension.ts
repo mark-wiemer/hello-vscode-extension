2ed325952a8af102dd830a2452947e00bb728c6f
Mark Wiemer <markwiemer@outlook.com>
2022-07-07 21:45:37 -0700

    Update to show 2022

--- src/extension.ts
+++ src/extension.ts
@@ -18,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
         () => {
             // The code you place here will be executed every time your command is executed
             // Display a message box to the user
-            vscode.window.showInformationMessage('Hello VS Code');
+            vscode.window.showInformationMessage('Hello VS Code 2022');
         },
     );
     const timeDisposable = vscode.commands.registerCommand(
